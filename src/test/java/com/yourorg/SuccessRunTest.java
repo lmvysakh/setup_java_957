e4c6a3706c24d1f089df22efd7938ce52b3c3ff7
lmvysakh <lmvysakh@github.com>
2025-11-21 13:46:49 +0530

    Checking for success

---
+++ src/test/java/com/yourorg/SuccessRunTest.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class SuccessRunTest {
+    @Test
+    public void testAlwaysPasses() {
+        assertTrue(true);
+    }
+}
